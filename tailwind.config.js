66b027e0489608ea277d03b7b8e09e5848b71db5
Coordinate-Cat <tetrapasta02@gmail.com>
2023-01-21 19:29:20 +0900

    feat: setting tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
 
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      padding: {
+        '15px': '15px',
+      },
+      width: {
+        90: '90%',
+      },
+      maxWidth: {
+        '1200px': '1200px',
+      },
+      screens: {
+        tablet: '769px',
+        // => @media (min-width: 769px) { ... }
+
+        laptop: '1201px',
+      },
+    },
   },
   plugins: [],
-}
+};

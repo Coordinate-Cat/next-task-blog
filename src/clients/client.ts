c1bc44f53efcf940b8ecbd91b7ebb1804787431d
Coordinate-Cat <tetrapasta02@gmail.com>
2023-01-26 10:02:06 +0900

    feat: nullish coalescing operator

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -4,5 +4,5 @@ import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
   serviceDomain: 'w2cy22awjv',
-  apiKey: process.env.API_KEY!,
+  apiKey: process.env.API_KEY ?? '',
 });
